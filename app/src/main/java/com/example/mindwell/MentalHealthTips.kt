a0eaa505b312bcc318aad506ff1a9bc1694bdffe
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-09-26 17:44:44 +0530

    MW | Mental health tips page

--- app/src/main/java/com/example/mindwell/MentalHealthTips.kt
+++ app/src/main/java/com/example/mindwell/MentalHealthTips.kt
@@ -1,20 +1,23 @@
 package com.example.mindwell
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mindwell.databinding.ActivityMentalHealthTipsBinding
 
 class MentalHealthTips : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMentalHealthTipsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_mental_health_tips)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMentalHealthTipsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Back button functionality
+        val backBtn = findViewById<ImageView>(R.id.backBtn)
+        backBtn.setOnClickListener {
+            finish() // Finish activity and return to previous one
         }
     }
 }
